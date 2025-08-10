5f99b1cc583133bb62591e5d0df6f0808c187ef1
Jeyavarma <jeyavarmaceg@gmail.com>
2025-08-10 16:25:20 +0530

    Add next-sitemap config and setup sitemap/robots.txt generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://mastery-sound-studios-9mnx.vercel.app/', 
+  generateRobotsTxt: true,          
+  sitemapSize: 5000,                
+ 
+};
